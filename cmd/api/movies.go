bb28ef03c1cd1a9569a9a06a505597247fe3b9f7
MohammadLashkari <moe.lashkari@gmail.com>
2024-07-06 09:52:50 +0330

    validate movie request input

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/MohammadLashkari/greenlight/internal/data"
+	"github.com/MohammadLashkari/greenlight/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -19,6 +20,17 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponce(w, r, err)
+		return
+	}
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+	v := validator.New()
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.faildValidationResponse(w, r, v.Errors)
 		return
 	}
 	fmt.Fprintf(w, "%+v\n", input)
