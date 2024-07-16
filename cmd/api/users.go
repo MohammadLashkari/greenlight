cafa77eb530d83e78d8f902e3f8a3030c8707417
MohammadLashkari <moe.lashkari@gmail.com>
2024-07-16 17:39:55 +0330

    sending emails in background

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -36,20 +36,24 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 	}
 	err = app.models.Users.Insert(user)
 	if err != nil {
-		if errors.Is(err, data.ErrDuplicateEmail) {
+		switch {
+		case errors.Is(err, data.ErrDuplicateEmail):
 			v.AddError("email", "a user with this email address already exists")
 			app.failedValidationResponse(w, r, v.Errors)
 			return
+		default:
+			app.serverErrorResponse(w, r, err)
+			return
 		}
-		app.serverErrorResponse(w, r, err)
-		return
 	}
-	err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-		return
-	}
-	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
+	app.background(
+		func() {
+			err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+			if err != nil {
+				app.logger.PrintError(err, nil)
+			}
+		})
+	err = app.writeJSON(w, http.StatusAccepted, envelope{"user": user}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
